c5a77f237e0053c90da869bc63fc6fa3ff2a5efa
Victor Turra <victor.florencio@univem.edu.br>
2024-06-13 20:13:50 -0300

    test: game should roll pins of all players

--- src/test/kotlin/GameTest.kt
+++ src/test/kotlin/GameTest.kt
@@ -41,4 +41,28 @@ class GameTest {
         // Check if the second player's frame has the roll
         assertEquals(9, players[0].getFrames()[0].sum())
     }
+
+    @Test
+    fun gameShouldRollPinsOfTheFirstAndTheSecondPlayer() {
+        val players = listOf(Player("Victor"), Player("Alice"))
+        val game = Game(players)
+
+        game.roll(5)
+        // Check if the first player's frame has the roll
+        assertEquals(players[0].getFrames()[0].sum(), 5)
+
+        game.roll(4)
+        // Check if the first player's frame sum is correct
+        assertEquals(players[0].getFrames()[0].sum(), 9)
+
+        game.roll(3)
+        // Check if the second player's frame has the roll
+        assertEquals(players[1].getFrames()[0].sum(), 3)
+
+        game.roll(3)
+        // Check if the second player's frame sum is correct
+        assertEquals(players[1].getFrames()[0].sum(), 6)
+    }
+
+    // gameShouldTurnToFirstPlayerAfterTheLastPlayerHasDone()
 }
